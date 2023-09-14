782e28e46a00041a6279ee05b965c12c6969b59e
Mark Balliet (PosiCat) <posicat@pobox.com>
2023-09-13 23:19:36 -0500

    A little bit more is working, lets me add the X10, but it doesn't load config yet.

--- __init__.py
+++ __init__.py
@@ -53,11 +53,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # Store an instance of the "connecting" class that does the work of speaking
     # with your actual devices.
 
-    e=entry
-    h=hass
-
-    _LOGGER.debug("Entry : " + str(e))
-    _LOGGER.debug("hass : " + str(h))
+    _LOGGER.debug("Entry : " + str(entry))
+    _LOGGER.debug("hass : " + str(hass))
 
     return True
 
@@ -66,31 +63,31 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
     
     success = True
 
-    # Data that you want to share with your platforms
-    hass.data[DOMAIN] = {}
-    x10_config = {}
-    x10_config[CONF_DEVICES] = {}
+    # # Data that you want to share with your platforms
+    # hass.data[DOMAIN] = {}
+    # x10_config = {}
+    # x10_config[CONF_DEVICES] = {}
     
-    for conf in config[DOMAIN]:
-        x10_config[CONFIG_USE_SSH] = conf.get(CONFIG_USE_SSH)
-        x10_config[CONFIG_SSH_HOST] = conf.get(CONFIG_SSH_HOST)
-        x10_config[CONFIG_SSH_USERNAME] = conf.get(CONFIG_SSH_USERNAME)
-        x10_config[CONFIG_SSH_PASSWORD] = conf.get(CONFIG_SSH_PASSWORD)
-        for device in conf.get(CONF_DEVICES):
-            type = device[CONF_TYPE]
+    # for conf in config[DOMAIN]:
+    #     x10_config[CONFIG_USE_SSH] = conf.get(CONFIG_USE_SSH)
+    #     x10_config[CONFIG_SSH_HOST] = conf.get(CONFIG_SSH_HOST)
+    #     x10_config[CONFIG_SSH_USERNAME] = conf.get(CONFIG_SSH_USERNAME)
+    #     x10_config[CONFIG_SSH_PASSWORD] = conf.get(CONFIG_SSH_PASSWORD)
+    #     for device in conf.get(CONF_DEVICES):
+    #         type = device[CONF_TYPE]
 
-            if (type == TYPE_APPLIANCE): # X10 calls them appliances, in HA they're switches
-                type = TYPE_SWITCH
-            if (not type in x10_config[CONF_DEVICES]):
-                x10_config[CONF_DEVICES][type] = []
+    #         if (type == TYPE_APPLIANCE): # X10 calls them appliances, in HA they're switches
+    #             type = TYPE_SWITCH
+    #         if (not type in x10_config[CONF_DEVICES]):
+    #             x10_config[CONF_DEVICES][type] = []
 
-            x10_config[CONF_DEVICES][type].append(device)
+    #         x10_config[CONF_DEVICES][type].append(device)
 
-    hass.data[DOMAIN] = x10_config;
-    # _LOGGER.debug("Domain: " + str(hass.data[DOMAIN]))
+    # hass.data[DOMAIN] = x10_config;
+    # # _LOGGER.debug("Domain: " + str(hass.data[DOMAIN]))
     
-    hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
-    hass.helpers.discovery.load_platform('light', DOMAIN, {}, config)
-    hass.helpers.discovery.load_platform('switch', DOMAIN, {}, config)
+    # hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+    # hass.helpers.discovery.load_platform('light', DOMAIN, {}, config)
+    # hass.helpers.discovery.load_platform('switch', DOMAIN, {}, config)
        
     return success
